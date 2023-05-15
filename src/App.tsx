12c667701392346405089960cbcd834c48763912
Andrew Grant <andrewgrant2009@hotmail.com>
2023-05-15 23:40:44 +0200

    Type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Todos from './components/Todos';
 import Todo from './models/todo';
 
 import "./App.css";
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [
@@ -15,7 +16,8 @@ new Todo('Build Pomodoro App'),
     <>
       <div>
         <h1>Hello welcome to the Pomodoro</h1>
-        <p>The Pomodoro Technique is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. Longer breaks, typically 15 to 30 minutes, are taken after four consecutive work intervals. Each work interval is called a pomodoro, the Italian word for tomato (plural: pomodori).</p>
+        <p className='text-card'>The Pomodoro Technique is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. Longer breaks, typically 15 to 30 minutes, are taken after four consecutive work intervals. Each work interval is called a pomodoro, the Italian word for tomato (plural: pomodori).</p>
+        <NewTodo />
         <Todos items={todos}/>
       </div>
     </>
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+
+const NewTodo = () => {
+  // useRef<HTMLInputElement> we need to tell typeScript exactly which element the ref will connect
+  // We need a starting value to show where the ref is linked (null)
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    // .current hold the current value
+    // current? is checking if useRef has an inital value.
+    // Use current! if you are sure it will have an inital connection
+    const enteredText = todoTextInputRef.current?.value;
+    // console.log(enteredText)
+
+    if (enteredText?.trim().length === 0) {
+      // Throe an error
+      return;
+    }
+    // Calling a function that is stored in App.tsx
+    // Pass pointers at functions as props to other components
+  };
+// console.log(todoTextInputRef.current?.value);
+
+
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
